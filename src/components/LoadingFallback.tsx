003d034d0da1454bf4146430a053393a1ce61485
lyntphust <plysn12092001@gmail.com>
2024-12-07 14:24:13 +0700

    Fix loading component

--- src/components/LoadingFallback.tsx
+++ src/components/LoadingFallback.tsx
@@ -1,28 +1,28 @@
 interface Props {
-  width?: number
-  height?: number
-  className?: string
-  show?: boolean
+  width?: number;
+  height?: number;
+  className?: string;
+  isLoading?: boolean;
 }
 
 export default function LoadingFallback({
   width,
   height,
-  className = '',
-  show = true,
+  className = "",
+  isLoading = true,
 }: Props) {
-  if (!show) {
-    return <></>
+  if (!isLoading) {
+    return <></>;
   }
 
   if (height && height <= 600) {
-    const numberOfLines = height ? Math.round(height / 60) : 1
+    const numberOfLines = height ? Math.round(height / 60) : 1;
 
     return (
       <div
         role="status"
         className={`mx-auto animate-pulse bg-white overflow-hidden ${className}`}
-        style={{ width: width ?? '100%', height: height ?? '100%' }}
+        style={{ width: width ?? "100%", height: height ?? "100%" }}
       >
         {Array.from({ length: numberOfLines }).map((_, index) => (
           <div
@@ -32,16 +32,16 @@ export default function LoadingFallback({
         ))}
         <span className="sr-only">Loading...</span>
       </div>
-    )
+    );
   }
 
   return (
     <div
       role="status"
       className={`mx-auto animate-pulse bg-gradient-to-b from-gray-200 to-gray-300 rounded-xl overflow-hidden ${className}`}
-      style={{ width: width ?? '100%', height: height ?? '100%' }}
+      style={{ width: width ?? "100%", height: height ?? "100%" }}
     >
       <span className="sr-only">Loading...</span>
     </div>
-  )
+  );
 }
